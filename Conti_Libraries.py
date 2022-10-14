f026095347f837bd7c4cfac8e094a19bf34f410c
EstelleRuellan <96437879+Nimpyy@users.noreply.github.com>
2022-10-14 10:25:43 -0400

    Add files via upload

---
+++ Conti_Libraries.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from collections import Counter
+import re
+import unidecode
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import TweetTokenizer
+from wordcloud.wordcloud import STOPWORDS
+from wordcloud import WordCloud
+import numpy as np
+import copy
+import pickle
+import re
+import math
+from textblob import TextBlob as tb 
+from tqdm import tqdm
+import string
+from spellchecker import SpellChecker
+import collections
